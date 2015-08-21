a103508eacbb4fe12bd4958a50be623eae8e1624
Max Eckard <eckardm@umich.edu>
2015-08-21 16:01:24 -0400

    adding initial human-readable outline

--- dacs-check.py
+++ dacs-check.py
@@ -1,1 +1,24 @@
+'''
+import what we need'''
 
+
+'''
+create list of required elements
+first for single-level and then for multi-level finding aids'''
+
+
+'''
+go through directory of eads
+distinguish between single-level and multi-level finding aids
+compare existing elements with list of required elements
+determine which required elements are missing'''
+
+
+'''
+export a csv file with filename and missing element
+first for single-level and then for multi-level finding aids'''
+
+
+'''
+print a report of what percentage of eads are dacs compliant
+first for single-level and then for multi-level finding aids'''
