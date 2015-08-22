001349b8feccadef6c7eb4698e0c93055b802e68
Max Eckard <eckardm@umich.edu>
2015-08-22 09:39:05 -0400

    started to add list for required fields

--- dacs-check.py
+++ dacs-check.py
@@ -3,15 +3,14 @@ import what we need'''
 
 
 '''
-create list of required elements
-first for single-level and then for multi-level finding aids'''
+create list of required elements'''
+
+# reference code (local identifier, repository identifier, country identifier); name and location of repository; title; date; extent; name of creator(s); scope and content; conditions governing access; languages and scripts of the material
+required_element_xpaths = [['ead/eadheader/eadid', 'ead/eadheader[@repositoryencoding]', 'ead/eadheader[@countryencoding]'], 'ead/eadheader/filedesc/publicationstmt/publisher', 'ead/eadheader/filedesc/titlestmt/titleproper',  
 
 
 '''
-go through directory of eads
-distinguish between single-level and multi-level finding aids
-compare existing elements with list of required elements
-determine which required elements are missing'''
+go through directory of eads, compare existing elements with list of required elements, determine which required elements are missing'''
 
 
 '''
